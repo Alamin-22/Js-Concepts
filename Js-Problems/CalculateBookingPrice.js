3536345ddedfb294e978ff6c1e962129a7ab751c
Mollik-Personal <mdalaminmollick@gmail.com>
2026-09-13 23:53:30 +0600

    dont know

--- Js-Problems/CalculateBookingPrice.js
+++ Js-Problems/CalculateBookingPrice.js
@@ -13,25 +13,33 @@ sun mon tue wed thurs fry sat
 const bookingCalculate = (nights, checkInDate = new Date()) => {
   const currentDate = checkInDate.getDate();
 
-  let calculatedCoast;
+  let calculatedCoast = 0;
 
-  for (const i = currentDate; i < nights; i++) {
-    const currentDay = i.getDay();
+  // 13 , 14 ,15 ,16 ,17 ,18 , 19
+  for (let i = currentDate; i < nights + currentDate; i++) {
+    const date = new Date(); // Sun Sep 13 , Mon Sep 14
+    date.setDate(i); // 1 , 2, 3, 4 ,5 ,6 , 7
+    let currentDay = date.getDay();
 
+    // if (currentDay === 0 || currentDay === 6) {
+    //   calculatedCoast = nights * prices.holiday;
+    // } else {
+    //   calculatedCoast = nights * prices.weekday;
+    // }
     if (currentDay === 0 || currentDay === 6) {
-      calculatedCoast = nights * prices.holiday;
+      calculatedCoast += prices.holiday;
     } else {
-      calculatedCoast = nights * prices.weekday;
+      calculatedCoast += prices.weekday;
     }
   }
 
   return calculatedCoast;
 };
 
-// const testCase1 = bookingCalculate(1, new Date("2026-11-11"));
-// const testCase2 = bookingCalculate(5, new Date("2026-11-07"));
+const testCase1 = bookingCalculate(1, new Date("2026-11-11"));
+const testCase2 = bookingCalculate(5, new Date("2026-11-07"));
 const testCase3 = bookingCalculate(7);
 
-// console.log("Test Case 1 => ", testCase1);
-// console.log("Test Case 2 => ", testCase2);
+console.log("Test Case 1 => ", testCase1);
+console.log("Test Case 2 => ", testCase2);
 console.log("Test Case 3 => ", testCase3);
