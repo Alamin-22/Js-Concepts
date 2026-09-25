51d5a23a1ac7e56d8d589bea4651736b6784eb15
Mollik-Personal <mdalaminmollick@gmail.com>
2026-09-25 23:05:16 +0600

    updated

--- Js-Problems/CalculateBookingPrice.js
+++ Js-Problems/CalculateBookingPrice.js
@@ -3,41 +3,28 @@ const prices = {
   holiday: 2200,
 };
 
-// in js week count start from 0 and end with 6 and in this case 0 means sunday and so on..
-
-/*
-sun mon tue wed thurs fry sat
- and according to this logic weekends date will be  0 for Sunday and 6 for saturday
- */
-
 const bookingCalculate = (nights, checkInDate = new Date()) => {
-  const currentDate = checkInDate.getDate();
+  const trackingDate = new Date(checkInDate);
 
-  let calculatedCoast = 0;
+  let calculatedCost = 0;
 
-  // 13 , 14 ,15 ,16 ,17 ,18 , 19
-  for (let i = currentDate; i < nights + currentDate; i++) {
-    const date = new Date(checkInDate); // Sun Sep 13 , Mon Sep 14
-    date.setDate(i); // 1 , 2, 3, 4 ,5 ,6 , 7
-    let currentDay = date.getDay();
+  for (let i = 0; i < nights; i++) {
+    const currentDay = trackingDate.getDay();
 
-    // if (currentDay === 0 || currentDay === 6) {
-    //   calculatedCoast = nights * prices.holiday;
-    // } else {
-    //   calculatedCoast = nights * prices.weekday;
-    // }
     if (currentDay === 0 || currentDay === 6) {
-      calculatedCoast += prices.holiday;
+      calculatedCost += prices.holiday;
     } else {
-      calculatedCoast += prices.weekday;
+      calculatedCost += prices.weekday;
     }
+
+    trackingDate.setDate(trackingDate.getDate() + 1);
   }
 
-  return calculatedCoast;
+  return calculatedCost;
 };
 
-const testCase1 = bookingCalculate(1, new Date("2026-11-11"));
-const testCase2 = bookingCalculate(5, new Date("2026-11-07"));
+const testCase1 = bookingCalculate(1, new Date("2026/11/11"));
+const testCase2 = bookingCalculate(5, new Date("2026/11/07"));
 const testCase3 = bookingCalculate(7);
 
 console.log("Test Case 1 => ", testCase1);
